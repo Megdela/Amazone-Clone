d62e80d3b2ba70bbe605ef0bf0ad9a92e0046a76
Megdela <megdela21@gmail.com>
2025-03-03 20:06:04 +0300

    Category

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import './App.css';
 import Header from './Components/Header/Header'
 import Carousel from './Components/Carousel/Carousel';
+import Category from './Components/Category/Category';
 
 function App() {
   return (
     <div className="App">
   <Header/>
   <Carousel/>
+  <Category/>
+
     </div>
   );
 }
---
+++ src/Components/Category/categoryFullInfos.js
@@ -0,0 +1,24 @@
+export const categoryInfos = [
+  {
+    title: "Electronics",
+    name: "electronics",
+    imgLink:
+      "https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1691075065521-galaxy M13.jpg",
+  },
+  {
+    title: "Discover fashion trends",
+    name: "women's clothing",
+    imgLink: "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
+  },
+  {
+    title: "Men's Clothing",
+    name: "men's clothing",
+    imgLink:
+      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+  },
+  {
+    title: "Jewelery",
+    name: "jewelery",
+    imgLink: "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
+  },
+];
